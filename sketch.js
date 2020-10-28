a85ebec0bec63a543208b3040144c542b8f81865
krushnabangar <72911900+krushnabangar@users.noreply.github.com>
2020-10-28 14:39:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+var fr,mr;
+
+
+
+function setup() {
+  createCanvas(1200,800);
+  fr = createSprite(400, 100, 50, 80);
+  fr.shapeColor = "green";
+  fr.debug = true;
+  mr = createSprite(400,800,80,30);
+  mr.shapeColor = "green";
+  mr.debug = true;
+
+  mr.velocityY = -5;
+  fr.velocityY = +5;
+
+}
+function draw(){
+  background(0);  
+  if(mr.x - fr.x < fr.width/2+mr.width/2 &&
+    fr.x - mr.x < fr.width/2+mr.width/2){
+      mr.velocityX = mr.velocityX *-1;
+      fr.velocityX = fr.velocityX *-1;
+    }
+    
+    
+  if(mr.y-fr.y < fr.height/2+mr.height/2 &&
+    fr.y - mr.y < fr.height/2 +mr.height/2){
+    fr.velocityY = fr.velocityY *-1;
+    mr.velocityY = mr.velocityY *-1;
+    }
+  drawSprites();
+}
